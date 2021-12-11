1a016e40dd25800dc475bd16330170b7448de96a
Veler Jevsky <43819843+Valetron@users.noreply.github.com>
2021-12-12 01:11:00 +0300

    Create passgen.sh

---
+++ passgen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+all_lower_forward="abcdefghjkmnpqrstuvwxyz"
+all_lower_backward="zyxwvutsrqpnmkjhgfedcba"
+all_upper_forward="ABCDEFGHJKMNPQRSTUVWXYZ"
+all_upper_backward="ZYXWVUTSRQPNMLKJHGFEDCBA"
+
+uniq_lower_forward="abcdefghijklmnopqrstuvwxyz"
+uniq_lower_backward="zyxwvutsrqponmlkjihgfedcba"
+uniq_upper_forward="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+uniq_upper_backward="ZYXWVUTSRQPONMLKJIHGFEDCBA"
+
+numbers_forward="1234567890"
+numbers_backward="0987654321"
+
+symbols="~!@#$№$%^&*()-_=+|?<>/\\"
+
+not_password=""
+
+echo $symbols
